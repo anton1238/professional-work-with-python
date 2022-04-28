3a7571ab8e958cb5640639b754f05ac6b6ad2463
anton1238 <92248698+anton1238@users.noreply.github.com>
2022-04-28 22:04:31 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,20 @@
+import unittest
+import user_service
+
+class TestYaApi(unittest.TestCase):
+
+    def setUp(self):
+        print('method setUp')
+
+    def test_success_create_folder(self):
+        self.assertEqual(user_service.create_folder('test'), 201)
+
+    def test_passed_create_folder(self):
+        self.assertEqual(user_service.create_folder('test_passed'), 409)
+
+    def tearDown(self):
+        user_service.delete_folder('test')
+        print('method tearDown')
+
+if __name__ == '__main__':
+    unittest.main()
